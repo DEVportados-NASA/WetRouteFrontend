d8b55d8e4ffcc5baf95c9352818ca7f8b9e6e4e7
LuisPineda05 <89112808+LuisPineda05@users.noreply.github.com>
2025-10-04 18:19:16 +0200

    a

--- src/app/shared/component/navbar/navbar.ts
+++ src/app/shared/component/navbar/navbar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import {MatTabChangeEvent} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +9,12 @@ import {Router} from '@angular/router';
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  tabs = ['/destination', '/activities', '/globe', '/about'];
+
   constructor(private router: Router) {}
 
-  navigate(path: string) {
-    this.router.navigate([path]);
+  onTabChange(event: MatTabChangeEvent) {
+    const route = this.tabs[event.index];
+    this.router.navigate([route]);
   }
-
 }
