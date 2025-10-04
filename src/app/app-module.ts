27b740886d5809937caadcc8cc5821a2e582de76
LuisPineda05 <89112808+LuisPineda05@users.noreply.github.com>
2025-10-04 17:01:22 +0200

    arreglando

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import {TitleComponent} from './views/title/title.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
